b3622b78d0962fb4a6ad983d7e2591a6203d8e2f
Dkingofcode <doladepo128@gmail.com>
2023-04-20 07:14:06 +0100

    Finished shopping Cart functionality and logic

---
+++ src/Cart/Cart.styles.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+
+export const Wrapper = styled.aside`
+   border: 2px solid;
+   font-family: Arial, Helvetica, sans-serif;
+   width: 500px;
+   padding: 20px;
+
+`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
